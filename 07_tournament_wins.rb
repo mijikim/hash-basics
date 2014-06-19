3bd458e27e4befaa570901096ad8454dca6bf25b
mijikim <miji.kim926@gmail.com>
2014-06-19 14:45:37 -0600

    Completed tournament exercise

--- 07_tournament_wins.rb
+++ 07_tournament_wins.rb
@@ -8,3 +8,8 @@ wins = {
     wimbledon: ["1976", "1977", "1978", "1979", "1980"]
   }
 }
+
+BJORN_BORG["wins"] = wins
+
+print "#{BJORN_BORG["wins"][:tournament_wins][:wimbledon][0]} and #{BJORN_BORG["wins"][:tournament_wins][:wimbledon][4]}"
+
