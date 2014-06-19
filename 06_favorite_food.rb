4263e5fd826e05a5a78f3a0f6791354eedac2d69
mijikim <miji.kim926@gmail.com>
2014-06-19 14:25:14 -0600

    Completed favorite food exercise

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,7 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+BJORN_BORG["favorite_foods"]
+
+print "Bjourn's favorite foods are #{BJORN_BORG["favorite_foods"][0]}, #{BJORN_BORG["favorite_foods"][1]}, #{BJORN_BORG["favorite_foods"][2]}"
