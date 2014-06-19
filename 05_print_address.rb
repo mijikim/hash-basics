bb34963dff85eb805b7ddc604c876ac7df408449
mijikim <miji.kim926@gmail.com>
2014-06-19 14:24:58 -0600

    Completed printing address

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,5 @@
 require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
+
+puts "#{BJORN_BORG["address"]["street"]}, #{BJORN_BORG["address"]["city"]}, #{BJORN_BORG["address"]["state"]}, #{BJORN_BORG["address"]["zipcode"]}"
